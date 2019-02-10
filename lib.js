41613c1e7ce34b94e2aff579b05916ec3ab3040e
gaidarjimax <gaidarjimax@gmail.com>
2019-02-10 21:15:07 +0200

    Added changes to the DOM lib

--- lib.js
+++ lib.js
@@ -1,5 +1,5 @@
 var get = ( selector ) => { 
-    
+
     if( ! ( typeof selector === 'string' || selector instanceof String ) ){
         return;
     }
@@ -16,41 +16,89 @@ var remove = ( target ) => {
     Array.isArray( el ) ? el.map( i => i.remove() ) : el.remove();
 }
 
-var createEl = ( tagName, props ) => 
+var createElement = ( tagName, props ) => 
 {
-    var newEl = document.createElement( tagName );
-
-    for( var key in props ){
-        if( key == 'text' ){
-            newEl.innerText = props[key];
-            continue;
-        }
-        
-        newEl.setAttribute( key, props[key] )
-    }
-
-    return newEl;
+    return setAttrsToElement( document.createElement( tagName ), props);
 }
 
-var append = ( target, tagName, props ) => {
-    
+var appendToElement = ( target, tagName, props ) => {
     var el = ( target instanceof Node || target instanceof HTMLElement ) ? target 
                                                                          : get( target );
 
-    Array.isArray( el ) ? el.map( i =>  append( i, tagName, props ) )
-                        : el.appendChild( createEl( tagName, props ) );
+    if( tagName == undefined || tagName == null ){
+        console.log( 'TagName invalid' );
+        return;
+    }
+
+    Array.isArray( el ) ? el.map( i =>  appendToElement( i, tagName, props ) )
+                        : el.appendChild( createElement( tagName, props ) );
 }
 
 var modify = ( target, props ) => {
-    var el = get( target );
-
-    for( var key in props ){
-        if( key == 'text' ){
-            el.innerText = props[key];
-            continue;
-        }
-        
-        el.setAttribute( key, props[key] )
+    var el = ( target instanceof Node || target instanceof HTMLElement ) ? target 
+                                                                         : get( target );
+    Array.isArray( el ) ? el.map( i =>  modify( i, props ) ) 
+                        : setAttrsToElement( el, props);  
+    return el;
+}
+var modifyStyles = ( target, props ) => {
+    var el;
+    if( target instanceof HTMLCollection ){
+        el = Array.from( target );
     }
+    else if( Array.isArray(target) ){
+        for( var item in target ){
+            if( ! ( item instanceof HTMLElement || item instanceof Node ) ) {
+                console.log( 'Array passed is not an Array of DOM Elements.' );
+                return;
+            }
+        }
+    }
+    else if( target instanceof Node || target instanceof HTMLElement ){
+        el = target;
+    }
+    else {
+        el = get( target );
+    }
+    // var el = ( target instanceof Node || target instanceof HTMLElement ) ? target 
+    //                                                                      : get( target );
+    Array.isArray( el ) ? el.map( i =>  modifyStyles( i, props ) ) 
+                        : setStylesToElement( el, props);  
+    return el;
 }
 
+var setAttrsToElement =( target, props )=>{
+
+    if(!(target instanceof Node || target instanceof HTMLElement)){
+        console.log('target is not an DOM element');
+        return;
+    }
+    for( var key in props ){
+        if (key =='innerText'){
+            target.innerText = props[key]; 
+            continue;
+        }
+        else if (key =='innerHTML'){
+            target.innerHTML = props[key]; 
+            continue;
+        }
+        target.setAttribute( key, props[key] )
+    }
+    
+    return target;
+}
+var setStylesToElement =( target, props )=>{
+
+    if(!(target instanceof Node || target instanceof HTMLElement)){
+        console.log('target is not an DOM element');
+        return;
+    }
+    for( var key in props ){
+        target.style[ key ] = props[ key ];
+    }
+    
+    return target;
+}
+//(()=>{
+//    doc ready
+//})();
